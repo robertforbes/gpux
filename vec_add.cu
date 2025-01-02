08d89d6f3a416f3f0e36344313f6b92256caed4c
Robert Forbes <robert.forbes@pentlandedge.com>
2025-01-02 19:15:04 +0000

    Updated the vec_add example to include some timing calculations.

--- vec_add.cu
+++ vec_add.cu
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <cuda.h>
+#include <chrono>
 
 #define N 10000
 
@@ -10,12 +11,18 @@ static void print_vec(float *vec, int n);
 // GPU functions.
 __global__ static void vec_add_gpu(float *out, float *x, float *y, int n);
 
+using std::chrono::high_resolution_clock;
+using std::chrono::duration_cast;
+using std::chrono::duration;
+using std::chrono::milliseconds;
+
 int main()
 {
     float x[N];
     float y[N];
     float cpu_out[N];
     float gpu_out[N];
+    duration<double, std::micro> us;
 
     for(int i = 0; i < N; i++)
     {
@@ -23,8 +30,12 @@ int main()
         y[i] = 23.0f;
     }
 
+    auto t0 = high_resolution_clock::now();
     vec_add(cpu_out, x, y, N);
-    printf("vec_add output CPU:\n");
+    auto t1 = high_resolution_clock::now();
+    us = t1 - t0;
+
+    printf("vec_add CPU, %10.5fus, output:\n", us.count());
     print_vec(cpu_out, 10);
 
     float *d_x;
@@ -37,17 +48,31 @@ int main()
     cudaMalloc((void**)&d_out, sizeof(float) * N);
 
     // Copy CPU -> GPU.
+    t0 = high_resolution_clock::now();
     cudaMemcpy(d_x, x, sizeof(float) * N, cudaMemcpyHostToDevice);
     cudaMemcpy(d_y, y, sizeof(float) * N, cudaMemcpyHostToDevice);
+    t1 = high_resolution_clock::now();
 
     // Run 1 block, 1 thread per block.
     vec_add_gpu<<<1,1>>>(d_out, d_x, d_y, N);
     cudaDeviceSynchronize();
+    auto t2 = high_resolution_clock::now();
 
     // Copy result GPU -> CPU.
     cudaMemcpy(gpu_out, d_out, sizeof(float) * N, cudaMemcpyDeviceToHost);
-
-    printf("vec_add output GPU:\n");
+    auto t3 = high_resolution_clock::now();
+   
+    // Calculate GPU durations.
+    duration<double, std::micro> d1, d2, d3, total;
+    d1 = t1 - t0;
+    d2 = t2 - t1;
+    d3 = t3 - t2;
+    total = t3 - t0;
+    printf("vec_add GPU, cp1 %10.5fus, ex %10.5fus, cp2 %10.5fus, total %10.5fus, output :\n",
+        d1.count(),
+        d2.count(),
+        d3.count(),
+        total.count());
     print_vec(gpu_out, 10);
 
     return 0;
