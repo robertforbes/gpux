c0565a062984c451318d1d7b719d3bb6fd072b6e
Robert Forbes <robert.forbes@pentlandedge.com>
2025-01-07 22:10:28 +0000

    Output more GPU information.

--- gpu_info.cu
+++ gpu_info.cu
@@ -21,29 +21,33 @@ int main()
 
 static void displayDeviceProperties(cudaDeviceProp *devProps)
 {
-    std::printf("name                  : %s\n", devProps->name);
-    std::printf("total global mem      : %zd\n", devProps->totalGlobalMem);
-    std::printf("shared mem per block  : %zd\n", devProps->sharedMemPerBlock);
-    std::printf("regs per block        : %d\n", devProps->regsPerBlock);
-    std::printf("warp size             : %d\n", devProps->warpSize);
-    std::printf("mem pitch             : %zd\n", devProps->memPitch);
-    std::printf("max threads per block : %d\n", devProps->maxThreadsPerBlock);
-    std::printf("max threads dim       : %d %d %d\n",
+    std::printf("name                          : %s\n", devProps->name);
+    std::printf("total global mem              : %zd\n", devProps->totalGlobalMem);
+    std::printf("shared mem per block          : %zd\n", devProps->sharedMemPerBlock);
+    std::printf("regs per block                : %d\n", devProps->regsPerBlock);
+    std::printf("warp size                     : %d\n", devProps->warpSize);
+    std::printf("mem pitch                     : %zd\n", devProps->memPitch);
+    std::printf("max threads per block         : %d\n", devProps->maxThreadsPerBlock);
+    std::printf("max threads dim               : %d %d %d\n",
         devProps->maxThreadsDim[0],
         devProps->maxThreadsDim[1],
         devProps->maxThreadsDim[2]);
-    std::printf("max grid size         : %d %d %d\n",
+    std::printf("max grid size                 : %d %d %d\n",
         devProps->maxGridSize[0],
         devProps->maxGridSize[1],
         devProps->maxGridSize[2]);
-    std::printf("clock rate            : %d\n", devProps->clockRate);
-    std::printf("total const mem       : %zd\n", devProps->totalConstMem);
-    std::printf("major                 : %d\n", devProps->major);
-    std::printf("minor                 : %d\n", devProps->minor);
-    std::printf("multiprocessor count  : %d\n", devProps->multiProcessorCount);
-    std::printf("tccDriver             : %d\n", devProps->tccDriver);
-    std::printf("async engine count    : %d\n", devProps->asyncEngineCount);
-    std::printf("memory clock rate     : %d\n", devProps->memoryClockRate);
-    std::printf("memory bus width      : %d\n", devProps->memoryBusWidth);
+    std::printf("clock rate                    : %d\n", devProps->clockRate);
+    std::printf("total const mem               : %zd\n", devProps->totalConstMem);
+    std::printf("major                         : %d\n", devProps->major);
+    std::printf("minor                         : %d\n", devProps->minor);
+    std::printf("multiprocessor count          : %d\n", devProps->multiProcessorCount);
+    std::printf("tccDriver                     : %d\n", devProps->tccDriver);
+    std::printf("async engine count            : %d\n", devProps->asyncEngineCount);
+    std::printf("memory clock rate             : %d\n", devProps->memoryClockRate);
+    std::printf("memory bus width              : %d\n", devProps->memoryBusWidth);
+
+    std::printf("reserved shared mem per block : %zd\n", devProps->reservedSharedMemPerBlock);
+    std::printf("unified addressing            : %d\n", devProps->unifiedAddressing);
+    std::printf("unified function pointers     : %d\n", devProps->unifiedAddressing);
 }
 
