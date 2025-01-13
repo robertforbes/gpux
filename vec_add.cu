97eaaae90874dfad2a1619b11f24b1e2d1ad9c0a
Robert Forbes <robert.forbes@pentlandedge.com>
2025-01-13 23:50:45 +0000

    Further experimentation with block sizes.

--- vec_add.cu
+++ vec_add.cu
@@ -2,7 +2,7 @@
 #include <cuda.h>
 #include <chrono>
 
-#define N 1000000
+#define N 1048576
 
 // CPU functions.
 static void vec_add(float *out, float *x, float *y, int n);
@@ -21,6 +21,7 @@ static float cpu_out[N];
 static float gpu_out0[N];
 static float gpu_out1[N];
 static float gpu_out2[N];
+static float gpu_out3[N];
 
 int main()
 {
@@ -47,6 +48,7 @@ int main()
     float *d_out0;
     float *d_out1;
     float *d_out2;
+    float *d_out3;
     
     // Allocate device memory.
     cudaMalloc((void**)&d_x, sizeof(float) * N);
@@ -54,6 +56,7 @@ int main()
     cudaMalloc((void**)&d_out0, sizeof(float) * N);
     cudaMalloc((void**)&d_out1, sizeof(float) * N);
     cudaMalloc((void**)&d_out2, sizeof(float) * N);
+    cudaMalloc((void**)&d_out3, sizeof(float) * N);
 
     // Copy CPU -> GPU.
     t0 = high_resolution_clock::now();
@@ -66,35 +69,43 @@ int main()
     cudaDeviceSynchronize();
     auto t2 = high_resolution_clock::now();
 
-    // Run 1 block, 100 threads per block.
-    vec_add_gpu<<<1,100>>>(d_out1, d_x, d_y, N);
+    // Run 1 block, 128 threads per block.
+    vec_add_gpu<<<1,128>>>(d_out1, d_x, d_y, N);
     cudaDeviceSynchronize();
     auto t3 = high_resolution_clock::now();
 
-    // Run 100 blocks, 1000 threads per block.
-    vec_add_gpu<<<100,1000>>>(d_out2, d_x, d_y, N);
+    // Run 128 blocks, 1024 threads per block.
+    vec_add_gpu<<<128,1024>>>(d_out2, d_x, d_y, N);
     cudaDeviceSynchronize();
     auto t4 = high_resolution_clock::now();
 
+    // Run 512 blocks, 256 threads per block.
+    vec_add_gpu<<<512,256>>>(d_out3, d_x, d_y, N);
+    cudaDeviceSynchronize();
+    auto t5 = high_resolution_clock::now();
+
     // Copy results GPU -> CPU.
     cudaMemcpy(gpu_out0, d_out0, sizeof(float) * N, cudaMemcpyDeviceToHost);
-    auto t5 = high_resolution_clock::now();
+    auto t6 = high_resolution_clock::now();
     cudaMemcpy(gpu_out1, d_out1, sizeof(float) * N, cudaMemcpyDeviceToHost);
     cudaMemcpy(gpu_out2, d_out2, sizeof(float) * N, cudaMemcpyDeviceToHost);
+    cudaMemcpy(gpu_out3, d_out3, sizeof(float) * N, cudaMemcpyDeviceToHost);
    
     // Calculate GPU durations.
-    duration<double, std::micro> d1, d2, d3, d4, d5;
+    duration<double, std::micro> d1, d2, d3, d4, d5, d6;
     d1 = t1 - t0;
     d2 = t2 - t1;
     d3 = t3 - t2;
     d4 = t4 - t3;
     d5 = t5 - t4;
-    printf("vec_add GPU, cp1 %10.5fus, ex1 %10.5fus, ex2 %10.5fus, ex3 %10.5fus, cp2 %10.5fus, output:\n",
+    d6 = t6 - t5;
+    printf("vec_add GPU, cp1 %10.5fus, ex1 %10.5fus, ex2 %10.5fus, ex3 %10.5fus, ex4 %10.5fus, cp2 %10.5fus, output:\n",
         d1.count(),
         d2.count(),
         d3.count(),
         d4.count(),
-        d5.count());
+        d5.count(),
+        d6.count());
 
     // Dump out part of the vectors, and run a check against the reference.
     print_vec(gpu_out0, 10);
@@ -103,6 +114,8 @@ int main()
     check_vec(gpu_out1, cpu_out, 0.00001, N);
     print_vec(gpu_out2, 10);
     check_vec(gpu_out2, cpu_out, 0.00001, N);
+    print_vec(gpu_out3, 10);
+    check_vec(gpu_out3, cpu_out, 0.00001, 1000);
 
     // Free device memory.
     cudaFree(d_x);
@@ -132,7 +145,11 @@ __global__ static void vec_add_gpu(float *out, float *x, float *y, int n)
     auto slice_len = n / (gridDim.x * blockDim.x);
     int start = blockIdx.x * block_len + threadIdx.x * slice_len;
     int end = start + slice_len;
-    // printf("block_len %d, slice_len %d\n", block_len, slice_len);
+    if(end > n)
+    {
+        end = n;
+    }
+    // printf("n %d, block_len %d, slice_len %d\n", n, block_len, slice_len);
     // printf("start %d, end %d\n", start, end);
 
     for(int i = start; i < end; i++)
