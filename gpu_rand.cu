ed03c029e8412706130e6d9492c8179aef48e202
Robert Forbes <robert.forbes@pentlandedge.com>
2025-03-21 12:17:44 +0000

    Added a simple test of generating random integers on the GPU.

--- gpu_rand.cu
+++ gpu_rand.cu
@@ -3,7 +3,7 @@
 #include <curand.h>
 #include <curand_kernel.h>
 
-__global__ void gpurand(curandState* state, uint64_t seed, float* rndVals, int n)
+__global__ void gpuRand(curandState* state, uint64_t seed, float* rndVals, int n)
 {
     int tid = 0;
     curand_init(seed, tid, 0, state);
@@ -13,21 +13,46 @@ __global__ void gpurand(curandState* state, uint64_t seed, float* rndVals, int n
     }
 }
 
+__global__ void gpuRandInt(
+    curandState* state,
+    uint64_t seed,
+    int* rndVals,
+    int n,
+    int min,
+    int max)
+{
+    int tid = 0;
+    curand_init(seed, tid, 0, state);
+    for(int i = 0; i < n; ++i)
+    {
+        float rndF = curand_uniform(state);
+        std::printf("curand_uniform %10.5f\n", rndF);
+        float scaled = (float)min + rndF * (float)(max - min);
+        int rndI = (int)round(scaled); 
+        std::printf("rand int %d\n", rndI);
+    }
+}
+
 int main()
 {
     curandState* state;
     float* devRndVals;
+    int* devIntVals;
     // float* hostRndVals;
     int nThreads = 1;
     int N = 10;
 
     cudaMalloc(&state, nThreads * sizeof(curandState));
     cudaMalloc(&devRndVals, N * sizeof(float));
+    cudaMalloc(&devIntVals, N * sizeof(int));
 
-    gpurand<<<1,1>>>(state, 123, devRndVals, N);
+    gpuRand<<<1,1>>>(state, 123, devRndVals, N);
+    gpuRandInt<<<1,1>>>(state, 123, devIntVals, N, 3, 7);
 
     cudaDeviceSynchronize();
     cudaFree(state);
     cudaFree(devRndVals);
+    cudaFree(devIntVals);
+
     return 0;
 }
