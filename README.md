e5160e6b6c6733219b4486e878e6afefec5e18e2
Robert Forbes <robert.forbes@pentlandedge.com>
2025-03-24 17:02:01 +0000

    Added a simple experiment with dynamic parallelism.

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ Examples can be built on the command line using the Nvidia compiler, e.g.
 nvcc  -Wno-deprecated-gpu-targets -o vec_add vec_add.cu
 nvcc  -Wno-deprecated-gpu-targets -o gpu_info gpu_info.cu
 ```
+The dynamic parallelism experiment requires an extra option:
+```
+nvcc  -Wno-deprecated-gpu-targets -rdc=true -o gpu_parent_child gpu_parent_child.cu
+```
---
+++ gpu_parent_child.cu
@@ -0,0 +1,25 @@
+// Simple experiment spawning children from a kernel function. 
+// Known as dynamic parallelism.
+#include <cstdio>
+#include <cuda.h>
+
+__global__ void child_kernel(int n) 
+{
+    std::printf("inside child kernel %d\n", threadIdx.x);
+}
+
+__global__ void parent_kernel(int n) 
+{
+    std::printf("inside parent kernel, about to create %d children\n", n);
+    child_kernel<<<1,n>>>(n);
+}
+
+int main()
+{
+    std::printf("Host process running, about to spawn parent kernel\n");
+    int n = 3;
+    parent_kernel<<<1,1>>>(n);
+    cudaDeviceSynchronize();
+    std::printf("Host process after sync\n");
+    return 0;
+}
